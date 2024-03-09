f7d033b7a0b597633eaaa7239a58b1429fca0b68
00seungho <tmdgh9910@naver.com>
2024-03-10 00:05:32 +0900

    1316

---
+++ 1316.py
@@ -0,0 +1,12 @@
+N = int(input())
+count = 0
+for i in range(N):
+    word = input()
+    wordDic = {}
+    for i in range(len(word)-1):
+        wordlist =[]
+        if word[i] == word[i+1]:
+            
+print(count)
+
+            
