08e6083c36fd6ecf22522dfed922c86f9ffd5231
00seungho <tmdgh9910@naver.com>
2024-03-10 16:27:36 +0900

    1316

--- 1316.py
+++ 1316.py
@@ -1,12 +1,27 @@
+def split_into_groups(word):
+    groups = []  
+    current_group = word[0] 
+    for char in word[1:]:
+        if char == current_group[-1]:
+            current_group += char
+        else:
+            groups.append(current_group) 
+            current_group = char  
+    groups.append(current_group)
+    return groups
+
+def is_group(gropus):
+    for i in range(len(gropus) - 1):
+        for j in range(i+1,len(gropus)):
+            if gropus[i][0] == gropus[j][0]:
+                return False
+    return True
 N = int(input())
 count = 0
 for i in range(N):
-    word = input()
-    wordDic = {}
-    for i in range(len(word)-1):
-        wordlist =[]
-        if word[i] == word[i+1]:
-            
+    words = input()
+    split_words=split_into_groups(words)
+    if is_group(split_words):
+        count +=1
 print(count)
-
             
